41fc64dd405b8afd61fb2d637bd170a49e0955a9
RaesakaDp <kakabuleng@gmail.com>
2023-08-24 14:01:55 +0700

    contact main

--- js/script.js
+++ js/script.js
@@ -43,3 +43,15 @@ function on() {
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "kakabuleng@gmail.com",
+    Password: "password",
+    To: "kakabuleng@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "This is the subject",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
+}
