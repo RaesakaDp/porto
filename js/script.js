82d9f10f2c404f30af6f7b341bb1eba3f6af2fdc
RaesakaDp <kakabuleng@gmail.com>
2023-09-25 14:39:10 +0700

    revisi/ganti navbar

--- js/script.js
+++ js/script.js
@@ -1,57 +1,133 @@
-// navbar
-$(".expandHome").mouseover(function () {
-  $(".sub-home").css({
-    display: "block",
-  });
-});
+(function () {
+  "use strict";
 
-$(".trapezoid")
-  .mouseleave(function () {
-    $(".trapezoid").css({
-      "margin-top": "-50px",
+  /**
+   * Easy Selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Easy On Scroll event Listener
+   */
+  constonscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
+
+  /**
+   * Navbar link active state on scroll
+   */
+  let navbarlinks = selectEl("#navbar .scrollto", true);
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200;
+    navbarlinks.forEach((navbarlink) => {
+      if (!navbarlink.hash) return;
+      let section = select(navbarlink.hash);
+      if (!section) return;
+      if (
+        position >= section.offsetTop &&
+        position <= section.offsetTop + section.offsetHeight
+      ) {
+        navbarlink.classList.add("active");
+      } else {
+        navbarlink.classList.remove("active");
+      }
     });
-    $(".sub-home").css({
-      display: "none",
+  };
+  window.addEventListener("load", navbarlinksActive);
+  onscroll(document, navbarlinksActive);
+
+  /**
+   * Scroll to an element with header offset
+   */
+  const scrollTo = (el) => {
+    let header = select("#header");
+    let offset = header.offsetHeight;
+
+    if (!header.classList.contains("header-scrolled")) {
+      offset -= 16;
+    }
+
+    let elementPos = select(el).offsetTop;
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: "smooth",
     });
-  })
-  .mouseenter(function () {
-    $(".trapezoid").css({
-      "margin-top": "0px",
-    });
-  });
+  };
+
+  /**
+   * Header fixed top on scroll
+   */
+  let selectHeader = select("#header");
+  if (selectHeader) {
+    let headerOffset = selectHeader.offsetTop;
+    let nextElement = selectHeader.nextElementSibling;
+    const headerFixed = () => {
+      if (headerOffset - window.scrollY <= 0) {
+        selectHeader.classList.add("fixed-top");
+        nextElement.classList.add("scrolled-offset");
+      } else {
+        selectHeader.classList.remove("fixed-top");
+        nextElement.classList.remove("scrolled-offset");
+      }
+    };
+    window.addEventListener("load", headerFixed);
+    onscroll(document, headerFixed);
+  }
+});
 
 /**
  * Hero type effect
  */
-const typed = select(".typed");
-if (typed) {
-  let typed_strings = typed.getAttribute("data-typed-items");
-  typed_strings = typed_strings.split(",");
-  new Typed(".typed", {
-    strings: typed_strings,
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 2000,
-  });
-}
+// const typed = select(".typed");
+// if (typed) {
+//   let typed_strings = typed.getAttribute("data-typed-items");
+//   typed_strings = typed_strings.split(",");
+//   new Typed(".typed", {
+//     strings: typed_strings,
+//     loop: true,
+//     typeSpeed: 100,
+//     backSpeed: 50,
+//     backDelay: 2000,
+//   });
+// }
 
-// Education overlay
-function on() {
-  document.getElementById("overlay").style.display = "block";
-}
-function off() {
-  document.getElementById("overlay").style.display = "none";
-}
+// // Education overlay
+// function on() {
+//   document.getElementById("overlay").style.display = "block";
+// }
+// function off() {
+//   document.getElementById("overlay").style.display = "none";
+// }
 
-function sendEmail() {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "kakabuleng@gmail.com",
-    Password: "password",
-    To: "kakabuleng@gmail.com",
-    From: document.getElementById("email").value,
-    Subject: "This is the subject",
-    Body: "And this is the body",
-  }).then((message) => alert(message));
-}
+// function sendEmail() {
+//   Email.send({
+//     Host: "smtp.gmail.com",
+//     Username: "kakabuleng@gmail.com",
+//     Password: "password",
+//     To: "kakabuleng@gmail.com",
+//     From: document.getElementById("email").value,
+//     Subject: "This is the subject",
+//     Body: "And this is the body",
+//   }).then((message) => alert(message));
+// }
