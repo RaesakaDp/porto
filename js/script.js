207d212179a63eefdc103726ed8fc7aa7ae06331
RaesakaDp <kakabuleng@gmail.com>
2025-10-24 23:03:30 +0700

    update education modern responsive

--- js/script.js
+++ js/script.js
@@ -100,3 +100,16 @@ function animateEducation() {
 
 window.addEventListener('scroll', animateEducation);
 window.addEventListener('load', animateEducation);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 80,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
